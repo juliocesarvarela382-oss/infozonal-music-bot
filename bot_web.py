969834bfd56da7f28d2be0d7c6881e6870fdc0a7
juliocesarvarela382-oss <juliocesarvarela382@gmail.com>
2026-09-22 18:47:16 -0300

    Update bot_web.py

--- bot_web.py
+++ bot_web.py
@@ -3,7 +3,7 @@ import re
 import json
 import requests
 from flask import Flask, request
-
+from music_database import conectar
 app = Flask(__name__)
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "").strip()
